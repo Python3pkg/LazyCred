390fcc2d9762868b26cba35f7620cdbc171cfe67
Val Tenyotkin <val@tenyotk.in>
2016-12-04 14:46:57 -0800

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 
 setup(
     name = 'lazycred',
-    version = '0.2.2',
+    version = '0.2.3',
     description = 'Minimalistic Credentials Manager based on AWS.',
     author = 'Val Tenyotkin',
     author_email = 'val@tenyotk.in',
     url = 'https://github.com/2deviant/LazyCred',
-    download_url = 'https://github.com/2deviant/LazyCred/tarball/0.2.2',
+    download_url = 'https://github.com/2deviant/LazyCred/tarball/0.2.3',
     packages = ['lazycred'],
     keywords = ['aws', 'kms', 'encryption', 'credentials', 'secret'],
     scripts = ['bin/lazycred'],
+    install_requires = ['boto', 'cryptography'],
+    license = 'MIT',
     classifiers = []
 )
