e42dcd7630e0f72373c49e99f5b150eb751d58f2
Val Tenyotkin <val@tenyotk.in>
2016-12-03 16:11:10 -0800

    Incrementing the minor version for the separation of the CLI tool and the library.  Incrementing the bug fix version for the method export fix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lazycred',
-    version='0.1.0',
+    version='0.2.1',
     description='Minimalistic Credentials Manager based on AWS.',
     author='Val Tenyotkin',
     author_email='val@tenyotk.in',
